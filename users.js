792664816302dd282fa923d84b9e7e6960a1401a
Bartłomiej Lipiec <bartek.lipiec2@gmail.com>
2021-05-25 14:52:14 +0200

    added profile image to database and read option

--- users.js
+++ users.js
@@ -15,7 +15,28 @@ allFriends.forEach((friend) => {
     })
 })
 
+$(".img-prof").click(function() {
+    $("#imgupload").trigger('click');
+})
+
+$("#imgupload").change(function(event) {
+    var image = document.getElementById('profileImg');
+    var file = event.target.files[0];
+    var formData = new FormData();
+    formData.append("id", "60a948a51c6f265f2c7ba943");
+    formData.append("profileImage", file);
+    image.src = URL.createObjectURL(event.target.files[0])
+    const config = {
+        headers: {
+            "content-type": "multipart/form-data"
+        }
+    };
+    axios.post(endpoint + "/photo", formData, config);
+})
+
+
 getUser("60a948a51c6f265f2c7ba943");
+
 
 function loadAllFriends(userid) {
     return repository.getFriendsByUserId(userid);
@@ -27,11 +48,14 @@ function getUser(idUser) {
     fetch(endpoint + "/" + idUser, {
         method: 'GET',
     }).then((resp) => resp.json()).then(response => {
+        var image = document.getElementById('profileImg');
         json = JSON.parse(JSON.stringify(response));
         user.id = json.id;
         user.email = json.email;
         user.name = json.name;
         user.surname = json.surname;
+        user.img = json.profileImage;
+        image.src = 'data:image/jpeg;base64,' + user.img.data;
         console.log(user);
         document.querySelector(".profile-username").textContent = user.name + " " + user.surname;
     });
